85514f8e0a53409e497904e07529a1118770e3c5
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2022-01-12 11:14:31 +0530

    unicokabol init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+library.add(
+    faGithub, faInstagram, faYoutube, faFacebook
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
