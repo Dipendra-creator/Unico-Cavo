935918a4fa744675ad853629742cf369c0703cd7
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2022-01-12 11:27:20 +0530

    minor bug

---
+++ pages/components/Navbar/NavBar.tsx
@@ -0,0 +1,18 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {UpperBar} from "../Upperbar/UpperBar";
+
+export function NavBar() {
+
+    return (
+        <>
+            {/*UPPER BAR*/}
+            <UpperBar />
+            {/*END OF UPPER BAR*/}
+
+            {/* START OF NAVBAR   */}
+
+        </>
+    )
+}
