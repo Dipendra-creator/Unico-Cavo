2bed8205519079d52933a58fb00dd8ad7fab6110
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2022-01-12 11:24:49 +0530

    minor bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {Navbar} from "./components/Navbar/Navbar";
+import {NavBar} from "./components/Navbar/NavBar";
 const Home: NextPage = () => {
   return (
     <>
-      <Navbar/>
+      <NavBar/>
     </>
   )
 }
