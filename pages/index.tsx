49c98dfd1f5d385ccb978188f492f3c5e14c68de
Aditya Sharma <55450440+dtr-beast@users.noreply.github.com>
2022-01-16 18:06:55 +0530

    Changed index.tsx (Lambda Function to Functional Component)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
-import type {NextPage} from 'next'
-import NavigationBar from "./components/Upperbar";
 import NavBar from "./components/Navbar";
 import Main from './components/Main';
 import Head from 'next/head'
 import Footer from "./components/Footer";
 
-const Home: NextPage = () => {
+export default function Home() {
     return (
         <>
             <Head>
                 <title>Unico Cavo</title>
-                <meta name="description" content="Unico Cavo ECommerce Next provides a way to quickly get up and running with a fully configurable ECommerce site using Next.js." />
-                <meta property="og:title" content="Unico Cavo " key="title" />
+                <meta name="description"
+                      content="Unico Cavo ECommerce Next provides a way to quickly get up and running with a fully configurable ECommerce site using Next.js."/>
+                <meta property="og:title" content="Unico Cavo " key="title"/>
             </Head>
             {/*<NavigationBar/>*/}
             <NavBar/>
@@ -20,5 +19,3 @@ const Home: NextPage = () => {
         </>
     )
 }
-
-export default Home
