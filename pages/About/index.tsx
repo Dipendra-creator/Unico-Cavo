81875e9ddcdbecd1bde18586c370f98c1edf33ad
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2022-01-19 14:38:55 +0530

    modified

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -16,7 +16,7 @@ export default function About () {
     d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"/>
                                 </svg>
                                 <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch
-                                    everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest
+                                    everyday carry neutra before they sold out fixie 90s microdosing. Tacos pinterest
                                     fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
                                 <a className="inline-flex items-center">
                                     <img alt="testimonial" src="https://dummyimage.com/106x106"
@@ -36,7 +36,7 @@ export default function About () {
     d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"/>
                                 </svg>
                                 <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch
-                                    everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest
+                                    everyday carry neutra before they sold out fixie 90s microdosing. Tacos pinterest
                                     fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
                                 <a className="inline-flex items-center">
                                     <img alt="testimonial" src="https://dummyimage.com/107x107"
@@ -64,58 +64,58 @@ export default function About () {
                         className="bg-indigo-100 rounded-full w-16 h-16 flex justify-center items-center text-indigo-500 shadow-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20"
                              fill="currentColor">
-                            <path fill-rule="evenodd"
-                                  d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                                  clip-rule="evenodd"/>
+                            <path d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"/>
                         </svg>
                     </div>
                     <h2 className="uppercase mt-6 text-indigo-500 font-medium mb-3"> Social conversations </h2>    <p
                     className="font-light text-sm text-gray-500 mb-3"> We get insulted by others, lose trust for those
-                    others. We get back stabbed by friends. It becomes harder for us to give others a hand. </p>    <a
-                    className="text-indigo-500 flex items-center hover:text-indigo-600" href="/"> More about us
+                    others. We get back stabbed by friends. It becomes harder for us to give others a hand. </p>
+                    <a
+                    className="text-indigo-500 flex items-center hover:text-indigo-600" href=""> More about us
                     icon <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
                               fill="currentColor">
-                        <path fill-rule="evenodd"
+                        <path
                               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                              clip-rule="evenodd"/>
+                              />
                     </svg></a></div>
                 <div className="p-8">
                     <div
                         className="bg-green-100 rounded-full w-16 h-16 flex justify-center items-center text-green-500 shadow-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20"
                              fill="currentColor">
-                            <path fill-rule="evenodd"
+                            <path
                                   d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                                  clip-rule="evenodd"/>
+                                  />
                         </svg>
                     </div>
                     <h2 className="uppercase mt-6 text-green-500 font-medium mb-3"> Social conversations </h2>    <p
                     className="font-light text-sm text-gray-500 mb-3"> We get insulted by others, lose trust for those
                     others. We get back stabbed by friends. It becomes harder for us to give others a hand. </p>    <a
-                    className="text-green-500 flex items-center hover:text-green-600" href="/"> More about us icon <svg
+                    className="text-green-500 flex items-center hover:text-green-600" href=""> More about us icon <svg
                     xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd"
+                    <path
                           d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                          clip-rule="evenodd"/>
+                          />
                 </svg></a></div>
                 <div className="p-8">
                     <div
                         className="bg-red-100 rounded-full w-16 h-16 flex justify-center items-center text-red-500 shadow-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20"
                              fill="currentColor">
-                            <path fill-rule="evenodd"
+                            <path
                                   d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                                  clip-rule="evenodd"/>
+                                  />
                         </svg>
                     </div>
                     <h2 className="uppercase mt-6 text-red-500 font-medium mb-3"> Social conversations </h2>    <p
                     className="font-light text-sm text-gray-500 mb-3"> We get insulted by others, lose trust for those
-                    others. We get back stabbed by friends. It becomes harder for us to give others a hand. </p>    <a
-                    className="text-red-500 flex items-center hover:text-red-600" href="/"> More about us icon <svg
+                    others. We get back stabbed by friends. It becomes harder for us to give others a hand. </p>
+                    <a
+                    className="text-red-500 flex items-center hover:text-red-600" href=""> More about us icon <svg
                     xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd"
+                    <path
                           d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                          clip-rule="evenodd"/>
+                          />
                 </svg></a></div>
             </div>
         </>
