ce3edaeefe24ab778d78380583dbd4c5ea0f1869
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2022-01-22 13:11:37 +0530

    modified

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -66,31 +66,31 @@ export default function About () {
                         <h6 className="text-3xl font-bold text-deep-purple-accent-400">
                             144K
                         </h6>
-                        <p className="font-bold">Downloads</p>
+                        <p className="font-bold">Customers </p>
                     </div>
                     <div className="text-center">
                         <h6 className="text-3xl font-bold text-deep-purple-accent-400">
-                            32.1K
+                            500+
                         </h6>
-                        <p className="font-bold">Users</p>
+                        <p className="font-bold">Products</p>
                     </div>
                     <div className="text-center">
                         <h6 className="text-3xl font-bold text-deep-purple-accent-400">
-                            12.9K
+                            100K+
                         </h6>
-                        <p className="font-bold">Subscribers</p>
+                        <p className="font-bold">Reviews</p>
                     </div>
                     <div className="text-center">
                         <h6 className="text-3xl font-bold text-deep-purple-accent-400">
-                            24.5K
+                            50+
                         </h6>
-                        <p className="font-bold">Cookies</p>
+                        <p className="font-bold">Quality Checks</p>
                     </div>
                 </div>
             </div>
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
-                    <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">Testimonials</h1>
+                    <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">VISION & MISSION</h1>
                     <div className="flex flex-wrap -m-4">
                         <div className="p-4 md:w-1/2 w-full">
                             <div className="h-full bg-gray-100 p-8 rounded">
@@ -99,17 +99,7 @@ export default function About () {
                                     <path
     d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"/>
                                 </svg>
-                                <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch
-                                    everyday carry neutra before they sold out fixie 90s microdosing. Tacos pinterest
-                                    fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
-                                <a className="inline-flex items-center">
-                                    <img alt="testimonial" src="https://dummyimage.com/106x106"
-                                         className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
-            <span className="flex-grow flex flex-col pl-4">
-              <span className="title-font font-medium text-gray-900">Holden Caulfield</span>
-              <span className="text-gray-500 text-sm">UI DEVELOPER</span>
-            </span>
-                                </a>
+                                <p className="leading-relaxed mb-6">To be an internationally reputed brand that delivers world class products and services in terms of their quality and the technology used, to ensure 100% consumer satisfaction without losing focus on its employee welfare.</p>
                             </div>
                         </div>
                         <div className="p-4 md:w-1/2 w-full">
@@ -119,17 +109,7 @@ export default function About () {
                                     <path
     d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"/>
                                 </svg>
-                                <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch
-                                    everyday carry neutra before they sold out fixie 90s microdosing. Tacos pinterest
-                                    fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
-                                <a className="inline-flex items-center">
-                                    <img alt="testimonial" src="https://dummyimage.com/107x107"
-                                         className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
-            <span className="flex-grow flex flex-col pl-4">
-              <span className="title-font font-medium text-gray-900">Alper Kamu</span>
-              <span className="text-gray-500 text-sm">DESIGNER</span>
-            </span>
-                                </a>
+                                <p className="leading-relaxed mb-6">To touching Lives and Empowering People with world class products and services in terms of their quality and the technology.</p>
                             </div>
                         </div>
                     </div>
@@ -139,9 +119,8 @@ export default function About () {
                 <div className="flex felx-col items-center justify-center"><span
                     className="rounded-full bg-indigo-500 px-2 py-1 text-white uppercase text-sm">      Insight    </span>
                 </div>
-                <h1 className="text-4xl font-medium text-gray-700 text-center mt-6"> Full-Funnel Social Analytics </h1>
-                <p className="text-center mt-6 text-lg font-light text-gray-500"> The time is now for it to be okay to
-                    be great. For being a bright color. For standing out. </p></div>
+                <h1 className="text-4xl font-medium text-gray-700 text-center mt-6">Quality</h1>
+                <p className="text-center mt-6 text-lg font-light text-gray-500"> Strict adherence to quality to make superior & incomparable products in the market forms the guiding principle towards every manufactured product. </p></div>
             <div className="grid grid-cols-1 md:grid-cols-3">
                 <div className="p-8">
                     <div
@@ -151,12 +130,11 @@ export default function About () {
                             <path d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"/>
                         </svg>
                     </div>
-                    <h2 className="uppercase mt-6 text-indigo-500 font-medium mb-3"> Social conversations </h2>    <p
-                    className="font-light text-sm text-gray-500 mb-3"> We get insulted by others, lose trust for those
-                    others. We get back stabbed by friends. It becomes harder for us to give others a hand. </p>
+                    <h2 className="uppercase mt-6 text-indigo-500 font-medium mb-3"> LED Laboratory </h2>    <p
+                    className="font-light text-sm text-gray-500 mb-3"> The lamp and lighting laboratory takes a medium to long range point of view to basic and applied research for new technology and new products. </p>
                     <a
                     className="text-indigo-500 flex items-center hover:text-indigo-600" href=""> More about us
-                    icon <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
                               fill="currentColor">
                         <path
                               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
@@ -172,10 +150,9 @@ export default function About () {
                                   />
                         </svg>
                     </div>
-                    <h2 className="uppercase mt-6 text-green-500 font-medium mb-3"> Social conversations </h2>    <p
-                    className="font-light text-sm text-gray-500 mb-3"> We get insulted by others, lose trust for those
-                    others. We get back stabbed by friends. It becomes harder for us to give others a hand. </p>    <a
-                    className="text-green-500 flex items-center hover:text-green-600" href=""> More about us icon <svg
+                    <h2 className="uppercase mt-6 text-green-500 font-medium mb-3"> Quality Assurance Division </h2>    <p
+                    className="font-light text-sm text-gray-500 mb-3"> The lamp and lighting laboratory takes a medium to long range point of view to basic and applied research for new technology and new products. </p>    <a
+                    className="text-green-500 flex items-center hover:text-green-600" href=""> More about us <svg
                     xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path
                           d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
@@ -191,11 +168,10 @@ export default function About () {
                                   />
                         </svg>
                     </div>
-                    <h2 className="uppercase mt-6 text-red-500 font-medium mb-3"> Social conversations </h2>    <p
-                    className="font-light text-sm text-gray-500 mb-3"> We get insulted by others, lose trust for those
-                    others. We get back stabbed by friends. It becomes harder for us to give others a hand. </p>
+                    <h2 className="uppercase mt-6 text-red-500 font-medium mb-3"> Design Centre </h2>    <p
+                    className="font-light text-sm text-gray-500 mb-3"> Based upon information provided by R & D department the Design Centre develops designs to meet quality lifestyle objectives. </p>
                     <a
-                    className="text-red-500 flex items-center hover:text-red-600" href=""> More about us icon <svg
+                    className="text-red-500 flex items-center hover:text-red-600" href=""> More about us <svg
                     xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path
                           d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
