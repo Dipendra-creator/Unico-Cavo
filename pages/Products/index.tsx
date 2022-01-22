797447a7b20a22d1a5a8d4a73f64cbfef863b795
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2022-01-22 20:50:29 +0530

    CSS modification in Products

--- pages/Products/index.tsx
+++ pages/Products/index.tsx
@@ -33,7 +33,7 @@ function Card(props: {
     return (
         <div className="shadow w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col" style={{height: "380px"}}>
             <a href="#">
-                <div className="hover:grow hover:shadow-lg" style={{justifyContent: "center", height: "252px"}}>
+                <div className="hover:grow hover:shadow-lg" style={{justifyContent: "center", height: 252, width: 252}}>
                     <Image width={252} height={252} src={props.imgSrc} alt=''/>
                 </div>
                 <div className="pt-3 flex items-center justify-between">
